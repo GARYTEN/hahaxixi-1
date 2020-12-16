72e096ce8f28a8dad4142da2e5b27948f763c557
nevancc <75403329+nevancc@users.noreply.github.com>
2020-12-16 09:31:13 +0800

    Add files via upload

--- jd_jdh.js
+++ jd_jdh.js
@@ -40,7 +40,7 @@ if ($.isNode()) {
 }
 const JD_API_HOST = 'https://api.m.jd.com/api';
 !(async () => {
-  $.newShareCodes = [];
+  $.newShareCodes = ['ba94051d8649edc945b0d255b36425c0','37c96e56c5e315b9e52459dcc3e08851'];
   if (!cookiesArr[0]) {
     $.msg($.name, '【提示】请先获取京东账号一cookie\n直接使用NobyDa的京东签到获取', 'https://bean.m.jd.com/', {"open-url": "https://bean.m.jd.com/"});
     return;
@@ -147,7 +147,7 @@ function queryShareInfo() {
           if (safeGet(data)) {
             data = JSON.parse(data);
             console.log(`您的分享助力码为：${data.data.shareParam}`)
-            $.newShareCodes.push(data.data.shareParam)
+            //$.newShareCodes.push(data.data.shareParam)
           }
         }
       } catch (e) {
