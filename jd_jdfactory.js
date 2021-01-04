c62fcc2acd4485b05ec52b96747b3393cdef6428
nevancc <75403329+nevancc@users.noreply.github.com>
2021-01-04 11:02:08 +0800

    Update jd_jdfactory.js

--- jd_jdfactory.js
+++ jd_jdfactory.js
@@ -346,7 +346,7 @@ async function doTask() {
 
 //领取做完任务的奖励
 function jdfactory_collectScore(taskToken) {
-  return new Promise(resolve => {
+  return new Promise(async resolve => {
     await $.wait(1030);
     $.post(taskPostUrl("jdfactory_collectScore", { taskToken }, "jdfactory_collectScore"), async (err, resp, data) => {
       try {
