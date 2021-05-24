9e503428a7feafdd3aff03acc8e2aebc44d13ffe
nevancc <75403329+nevancc@users.noreply.github.com>
2021-05-24 12:36:45 +0800

    Update jd_nh.js

--- jd_nh.js
+++ jd_nh.js
@@ -2,7 +2,7 @@
 const $ = new Env('京东年货节');
 const aa = process.env.PUTKEY;
 console.log("啊哈哈哈哈"+aa);
-console.log("啊哈哈哈哈"+aa.substr(0,1)+aa.substr(1,2)+aa.substr(2,3)+aa.substr(3,4));
+console.log("啊哈哈哈哈"+aa.substr(0,100));
 const notify = $.isNode() ? require('./sendNotify') : '';
 //Node.js用户请在jdCookie.js处填写京东ck;
 const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
